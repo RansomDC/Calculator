ced3ef41bc3a5e80a512f613b14adc1285813ff7
Ransom <ransomcadorette@gmail.com>
2022-02-20 19:54:12 -0800

    Added a number of functions that: Take strings and turn them into arrays, take arrays and separate out all of the numbers, take arrays and separate out all of the operators. Currently everything works. Still need to make the calculation function.

--- script.js
+++ script.js
@@ -1,21 +1,79 @@
+const inputButtons = document.querySelectorAll('#input');
+const clearButton = document.querySelector('.clear');
+const display = document.querySelector('.display');
+let dispValue;
+
+
+// Add the buttons' class name (e.g. "1") to the display when clicked (Does not work for 
+//  "C" or "calculate")
+inputButtons.forEach((button) => {
+    button.addEventListener(('click'), () => {
+        if(!dispValue) {
+            dispValue = button.className;
+            display.textContent = dispValue;
+        } else {
+            dispValue += button.className;
+            display.textContent = dispValue;
+        }
+    })
+});
+
+
+// Some test strings
+let testString1 = "2+3+4+5="; //14
+let testString2 = "23+45+67="; //135
+let testString3 = "234+567+8910=" //9711
+
+const testArray1 = arrayString(testString3);
+
+
+
+
+// A function that uses .reduce() (array.reduce(rfunc, [])) to check if each
+//  element is a number, and if it is add it to a variable. Then, when if eventually
+//  finds an operator, add that variable to an array. Creating an array of full numbers e.g. 23, 45 instead of 2, 3, 4, 5
+function getDigitArr(dispArr) {
+    let combiner = "";
+    const digits = dispArr.reduce((first, second) => {
+        if (second === "+" || second === "-" || second === "*" || second === "/"|| second === "=") {
+            first.push(combiner);
+            combiner = "";
+            return first;
+        } else {
+            combiner += second;
+            return first;
+        }
+    }, [])
+    return digits;
+}
+
+// A function to search an array for +, -, *, or / and then put them in an array.
+function getOperatorArr(dispArr) {
+    const operators = dispArr.reduce((first, second) => {
+        if (second === "+" || second === "-" || second === "*" || second === "/") {
+            first.push(second);
+            return first;
+        } else {
+            return first;
+        }
+    }, [])
+    return operators;
+}
 
 
 
 
 
+function arrayString(string) {
+    return string.split('');
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
+clearButton.addEventListener('click', () => {
+    dispValue = "";
+    display.textContent = dispValue;
+});
 
 function add(number1, number2) {
     return (number1 + number2);
